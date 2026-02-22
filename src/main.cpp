b61ac660d64cc3cf9f3b7352fb91a37e74c1c8e3
aakashsingghh <akashsingh33280@gmail.com>
2026-02-22 23:38:00 +0530

    implement setup and loop functions in main.cpp for sensor initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 int fsrPin = A0;
-// void setup() {
-//     Serial.begin(9600);
-//     int fsrValue = analogRead(fsrPin);
-//     Serial.println(fsrValue);
-//     delay(500);
-//     // write your initialization code here
-// }
-//
-// void loop() {
-//
-//     // write your code here
-// }
+void setup() {
+    Serial.begin(9600);
+    int fsrValue = analogRead(fsrPin);
+    Serial.println(fsrValue);
+    delay(500);
+    // write your initialization code here
+}
+
+void loop() {
+
+    // write your code here
+}
